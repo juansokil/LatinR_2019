9510e91a74d1d637ba161d663ce72724162924ac
Juan Pablo Sokil <juanpablosokil@gmail.com>
2019-01-03 08:58:58 -0300

    Update script.R

--- 02-Scripts/script.R
+++ 02-Scripts/script.R
@@ -29,18 +29,44 @@ base_completa = unique(completo %>%
                          select(UT, AF, TI, SO, AB, C1, PY, WC))
 
 
+
 ###Genera base autores###
 autores <- base_completa %>% 
-  select(UT, AF) %>% 
+  select(UT, PY, AF) %>% 
   separate_rows(AF, sep=';')  
+
+View(autores)
 
 autores <- separate(autores, AF, into=c("apellido", "nombre"), sep=',', remove = FALSE)
 autores$nombre <- str_trim(autores$nombre, side = c("both"))
 autores <- separate(autores, nombre, into=c("primer_nombre", "segundo_nombre"), sep=' ', remove = FALSE)
 autores$primer_nombre <- str_trim(autores$primer_nombre, side = c("both"))
+
 gender_df <- gender(autores$primer_nombre)
 authors <-   unique(left_join(autores, gender_df, by = c("primer_nombre" = "name")))
+authors2 <- authors[-1]
+authors_unique <- unique(authors2)
 
+
+tabla <- authors_unique %>%
+  group_by(PY, gender) %>%
+  count()
+
+View(tabla)
+
+
+
+
+
+#####################PARTICIPACION##################
+###### AUTORES TOTALES######
+author_share <- authors %>%
+  filter(gender =='male' | gender=='female') %>%
+  select(UT) %>%
+  group_by(UT) %>%
+  mutate(author = n())
+
+         ########PARTICIPACION######
 author_male <- authors %>%
   filter(gender =='male') %>%
   select(gender, UT) %>%
@@ -53,11 +79,41 @@ author_female <- authors %>%
   group_by(UT, gender) %>%
   mutate(author_female = n())
 
+base_completa <- left_join(base_completa, author_share, by = c("UT" = "UT") )
 base_completa <- left_join(base_completa, author_male, by = c("UT" = "UT") )
 base_completa <- left_join(base_completa, author_female, by = c("UT" = "UT") )
 
-table(base_completa$author_female)
-table(base_completa$author_male)
+base_completa <- unique(base_completa)
+
+
+View(base_completa)
+
+base_completa %>% 
+  filter(gender.x =='male') %>% 
+  group_by(PY) %>% 
+  summarize(Unique_Elements = n_distinct(UT), promedio=mean(author_male))
+
+
+base_completa %>% 
+  filter(gender.y =='female') %>% 
+  group_by(PY) %>% 
+  summarize(Unique_Elements = n_distinct(UT), promedio=mean(author_female))
+
+
+table(base_completa$PY)
+
+
+base_completa %>% 
+  filter(gender.x =='male') %>% 
+  group_by(PY) %>% 
+  summarize(Unique_Elements = n_distinct(UT))
+
+   
+table(base_completa$author_female, base_completa$PY)
+table(base_completa$author_male, base_completa$PY)
+
+
+
 
 ####ANALISIS BIBLIOMETRICO####
 #https://cran.r-project.org/web/packages/bibliometrix/vignettes/bibliometrix-vignette.html
